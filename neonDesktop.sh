088232824d22e700268f29b614b4460897719fb8
john-jparker <findingpartners115@gmail.com>
2022-08-20 20:56:56 -0400

    Update neonDesktop.sh

--- neonDesktop.sh
+++ neonDesktop.sh
@@ -19,7 +19,7 @@ function yes_or_no {
 echo "${GREEN} Updating${NC}"
 sudo pkcon update -y
 
-echo "${GREEN} not install nvidia driver 515${NC}
+echo "${GREEN} not install nvidia driver 515${NC}"
 yes_or_no "$message" && sudo apt-get install nvidia-driver-515 -y
       
 echo "${GREEN} Installing GO${NC}"
