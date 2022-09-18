bc1361ec53570da27dd8953824211e324c1c89ca
john-jparker <findingpartners115@gmail.com>
2022-09-18 17:02:49 -0500

    Update neonDesktop.sh

--- neonDesktop.sh
+++ neonDesktop.sh
@@ -20,56 +20,20 @@ echo "${GREEN} Updating${NC}"
 sudo pkcon update -y
 sudo add-apt-repository ppa:neovim-ppa/unstable
 
-echo "${GREEN} install nvidia driver 515?${NC}"
+echo "${GREEN} Install nvidia driver 515?${NC}"
 yes_or_no "$message" && sudo apt-get install nvidia-driver-515 -y
       
-echo "${GREEN} Installing GO${NC}"
-curl -fsSLo- https://s.id/golang-linux | bash
-echo 'GOROOT="/home/$USER/go"' >> .bashrc
-echo 'GOPATH="/home/$USER/go/packages"' >> .bashrc
-echo 'PATH=$PATH:$GOROOT/bin:$GOPATH/bin' >> .bashrc
-
-wget https://github.com/tinygo-org/tinygo/releases/download/v0.25.0/tinygo_0.25.0_amd64.deb
-sudo dpkg -i tinygo_0.25.0_amd64.deb
-export PATH=$PATH:/usr/local/bin
-
-echo "${GREEN} Installing Rust, rustc, cargo${NC}"
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
-
-echo "${GREEN} Installing Node 17 & NPM yarn${NC}"
-curl -sL https://deb.nodesource.com/setup_current.x | sudo -E bash -
-sudo apt-get install -y nodejs
-sudo npm install -g nodemon yarn
-
-echo "${GREEN} Installing AWS CLI${NC}"
-curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-unzip awscliv2.zip
-sudo ./aws/install
-
-echo "${GREEN} Installing GCP CLI${NC}"
-sudo apt-get install apt-transport-https ca-certificates gnupg -y
-echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
-sudo curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
-sudo apt update -y
-sudo apt-get install google-cloud-cli -y
-
-echo "${GREEN} Installing Docker${NC}"
-sudo mkdir -p /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update -y
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
+echo "${GREEN} Snap installing GO Rust Node aws-cli google-cloud-cli${NC}"
+sudo snap install go --classic 
+sudo snap install rustup --classic 
+sudo snap install node --classic 
+sudo snap install aws-cli --classic 
+sudo snap install google-cloud-cli --classic 
 
 echo "${GREEN} Downloading github projects to /github dir${NC}"
 git config --global user.name "trevor256"
 git config --global user.email "256trevor@gmail.com"
-git clone https://github.com/trevor256/trevor256.com.git github/trevor256.com/
-git clone https://github.com/trevor256/MyConfig.git github/MyConfig/
-git clone https://github.com/trevor256/BSFSB github/BSFSB/
-git clone https://github.com/trevor256/erupt-0.2.git github/erupt-0.2/
-chmod -R 777 github
+mkdir gh
 
 echo "${GREEN} Installing starship and nerdfonts${NC}"
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Hack.zip
@@ -78,12 +42,11 @@ fc-cache -fv
 curl -sS https://starship.rs/install.sh | sh -s -- -y
 echo 'eval "$(starship init bash)"' >> .bashrc
 
-echo "${GREEN} Install Xpen${NC}"
-curl https://www.xp-pen.com/download/file/id/1949/pide819/ext/deb.html -o xpen.deb
-sudo dpkg -i xpen.deb
+echo "${GREEN} Install Huion${NC}"
+curl https://driverdl.huion.com/driver/Linux/HuionTablet_v15.0.0.89.202205241352.x86_64.deb -o huion.deb && sudo dpkg -i huion.deb
 
-echo "${GREEN} apt install ffmpeg build-essential openjdk-17-jdk openjdk-17-jre python3-pip python3-virtualenv${NC}"
-sudo apt-get install neovim ffmpeg build-essential openjdk-17-jdk openjdk-17-jre python3-pip python3-virtualenv rustc cargo -y
+echo "${GREEN} apt install docker ffmpeg build-essential openjdk-17-jdk openjdk-17-jre python3-pip python3-virtualenv${NC}"
+sudo apt-get install docker.io neovim ffmpeg build-essential openjdk-17-jdk openjdk-17-jre python3-pip python3-virtualenv rustc cargo -y
 echo "${GREEN} flatpak install nvim kdenlive Blender LibreOffice Discord Inkscape krita Godot VideoDownloader Audacity Minecraft obs${NC}"
 sudo flatpak install flathub org.kde.kdenlive org.blender.Blender org.libreoffice.LibreOffice com.discordapp.Discord org.kde.krita org.godotengine.GFodot -y 
 sudo flatpak install org.inkscape.Inkscape com.github.unrud.VideoDownloader org.audacityteam.Audacity com.atlauncher.ATLauncher com.obsproject.Studio -y
