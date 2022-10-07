9822c196962a34b8ecc5dde899519c17c10ad1f5
john-jparker <findingpartners115@gmail.com>
2022-10-07 13:14:59 -0500

    Update neonDesktop.sh

--- neonDesktop.sh
+++ neonDesktop.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 #  Author https://github.com/trevor256
-#  script configs new system
-#  Run on Linux (KDE NEON)
-#  make a usb   sudo dd bs=4M if=Downloads/neon.iso of=/dev/sd<?> conv=fdatasync status=progress
+#  Run on Linux (KDE NEON) https://neon.kde.org/download
+#  make a usb     sudo dd bs=4M if=Downloads/neon.iso of=/dev/sd<?> conv=fdatasync status=progress 
 GREEN="$(tput setaf 2)"
 NC="$(tput sgr0)"
 
@@ -17,16 +16,15 @@ function yes_or_no {
 }
 
 echo "${GREEN} Updating${NC}"
-sudo pkcon update -y
+curl -sL https://deb.nodesource.com/setup_18.x | sudo bash -
 sudo add-apt-repository ppa:neovim-ppa/unstable
+sudo pkcon update -y
 
 echo "${GREEN} Install nvidia driver 515?${NC}"
 yes_or_no "$message" && sudo apt-get install nvidia-driver-515 -y
       
-echo "${GREEN} Snap installing GO Rust Node aws-cli google-cloud-cli${NC}"
+echo "${GREEN} Snap installing GO aws-cli google-cloud-cli${NC}"
 sudo snap install go --classic 
-sudo snap install rustup --classic 
-sudo snap install node --classic 
 sudo snap install aws-cli --classic 
 sudo snap install google-cloud-cli --classic 
 
@@ -45,8 +43,8 @@ echo 'eval "$(starship init bash)"' >> .bashrc
 echo "${GREEN} Install Huion${NC}"
 curl https://driverdl.huion.com/driver/Linux/HuionTablet_v15.0.0.89.202205241352.x86_64.deb -o huion.deb && sudo dpkg -i huion.deb
 
-echo "${GREEN} apt install docker ffmpeg build-essential openjdk-17-jdk openjdk-17-jre python3-pip python3-virtualenv${NC}"
-sudo apt-get install docker.io neovim ffmpeg build-essential openjdk-17-jdk openjdk-17-jre python3-pip python3-virtualenv rustc cargo -y
+echo "${GREEN} apt install nodejs docker ffmpeg build-essential openjdk-17-jdk openjdk-17-jre python3-pip python3-virtualenv${NC}"
+sudo apt-get install nodejs docker.io neovim ffmpeg build-essential openjdk-17-jdk openjdk-17-jre python3-pip python3-virtualenv rustc cargo -y
 echo "${GREEN} flatpak install nvim kdenlive Blender LibreOffice Discord Inkscape krita Godot VideoDownloader Audacity Minecraft obs${NC}"
 sudo flatpak install flathub org.kde.kdenlive org.blender.Blender org.libreoffice.LibreOffice com.discordapp.Discord org.kde.krita org.godotengine.GFodot -y 
 sudo flatpak install org.inkscape.Inkscape com.github.unrud.VideoDownloader org.audacityteam.Audacity com.atlauncher.ATLauncher com.obsproject.Studio -y
