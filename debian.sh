849a333fd938be34a3133d40059862f54c95fb80
john-jparker <findingpartners115@gmail.com>
2022-11-04 21:05:45 -0500

    Update debian.sh

--- debian.sh
+++ debian.sh
@@ -10,18 +10,13 @@ NONE="$(tput sgr0)"
 
 echo "${GREEN} Adding nonfree repositories and updating${NONE}"
 sudo apt-add-repository non-free
+curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
 sudo apt-get update -y && sudo apt-get upgrade
 
 echo "${GREEN} Install nvidia driver${NONE}"
 sudo apt install nvidia-detect
 sudo apt install nvidia-driver
 
-echo "${GREEN} Snap install GO, aws-cli, google-cloud-cli, nvim${NONE}"
-sudo apt install snapd
-systemctl status snapd
-sudo snap install core
-sudo snap install go --classic 
-sudo snap install aws-cli --classic 
 sudo snap install google-cloud-cli --classic 
 
 echo "${GREEN} Downloading github projects to /github dir${NONE}"
@@ -39,5 +34,23 @@ sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub
 echo "${GREEN} Flatpak install kdenlive, Blender, LibreOffice, Discord, Boxes, Inkscape, Krita, postman, Godot, Audacity, ATMinecraft, OBS${NONE}"
 sudo flatpak install -y flathub org.kde.kdenlive org.blender.Blender org.libreoffice.LibreOffice com.discordapp.Discord org.gnome.Boxes md.obsidian.Obsidian \
 com.getpostman.Postman org.kde.krita org.godotengine.GFodot org.inkscape.Inkscape org.audacityteam.Audacity com.atlauncher.ATLauncher com.obsproject.Studio
-       
-init 6
+
+echo "${GREEN} golang ${NONE}"
+sudo curl -L https://go.dev/dl/go1.19.3.linux-amd64.tar.gz -o /usr/local/go19.tar.gz
+sudo tar -zxvf /usr/local/go19.tar.gz
+echo "export PATH=/usr/local/go/bin:${PATH}" | sudo tee /etc/profile.d/go.sh
+source /etc/profile.d/go.sh
+echo "export PATH=/usr/local/go/bin:${PATH}" | sudo tee -a $HOME/.profile
+source $HOME/.profile 
+ 
+echo "${GREEN} aws-cli ${NONE}"
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install 
+
+echo "${GREEN} gcp-cli ${NONE}"
+curl -O https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-cli-408.0.1-linux-x86_64.tar.gz
+tar -xf google-cloud-cli-408.0.1-linux-x86_64.tar.gz
+./google-cloud-sdk/install.sh
+
+sudo reboot
