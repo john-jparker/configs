c8eae671eb5f3f38b2f0430317753a4839b27521
john-jparker <findingpartners115@gmail.com>
2022-11-05 10:03:06 -0500

    Update debian.sh

--- debian.sh
+++ debian.sh
@@ -9,9 +9,15 @@ GREEN="$(tput setaf 2)"
 NONE="$(tput sgr0)"
 
 echo "${GREEN}nonfree repositories and updating${NONE}"
+sudo apt install wget
 sudo apt-add-repository non-free
+sudo apt-add-repository contrib
+echo "deb https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
 curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
-sudo apt-get update -y && sudo apt-get upgrade
+wget https://developer.download.nvidia.com/compute/cuda/11.8.0/local_installers/cuda-repo-debian11-11-8-local_11.8.0-520.61.05-1_amd64.deb && sudo dpkg -i cuda-repo-debian11-11-8-local_11.8.0-520.61.05-1_amd64.deb
+sudo cp /var/cuda-repo-debian11-11-8-local/cuda-*-keyring.gpg /usr/share/keyrings/
+sudo apt-get update -y && sudo -y apt-get upgrade
+
 
 echo "${GREEN}git config${NONE}"
 git config --global user.name "trevor256"
@@ -19,22 +25,23 @@ git config --global user.email "256trevor@gmail.com"
 mkdir Desktop/gh
 
 echo "${GREEN}nvidia drivers${NONE}"
-sudo apt install nvidia-detect
-sudo apt install nvidia-driver
+sudo apt install -y nvidia-detect
+sudo apt install -y nvidia-driver
+sudo apt-get -y install cuda
 
 echo "${GREEN}Huion tablet driver${NONE}"
 curl https://driverdl.huion.com/driver/Linux/HuionTablet_v15.0.0.89.202205241352.x86_64.deb -o huion.deb && sudo dpkg -i huion.deb
 
 echo "${GREEN}install nodejs, ffmpeg, build-essential shellcheck${NONE}"
-sudo apt-get install -y flatpak nodejs ffmpeg build-essential shellcheck pulseaudio pulseaudio-module-bluetooth pavucontrol bluez-firmware
+sudo apt-get install -y flatpak nodejs ffmpeg build-essential shellcheck pulseaudio google-cloud-cli /
+pulseaudio-module-bluetooth pavucontrol bluez-firmware apt-transport-https ca-certificates gnupg
 sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
-echo "${GREEN}Flatpak install Blender, LibreOffice, Discord, Boxes, Inkscape, Krita, postman, Godot, ATMinecraft, OBS${NONE}"
+echo "${GREEN}Flatpak install Blender, Discord, Boxes, Inkscape, Krita, postman, Godot, ATMinecraft, OBS${NONE}"
 sudo flatpak install -y flathub org.blender.Blender \
 com.discordapp.Discord org.gnome.Boxes md.obsidian.Obsidian \
 com.getpostman.Postman org.kde.krita org.godotengine.GFodot \
-org.inkscape.Inkscape com.atlauncher.ATLauncher \
-com.obsproject.Studio org.libreoffice.LibreOffice
+org.inkscape.Inkscape com.atlauncher.ATLauncher com.obsproject.Studio \
 
 echo "${GREEN}golang ${NONE}"
 wget https://golang.org/dl/go1.19.3.linux-amd64.tar.gz
@@ -48,8 +55,3 @@ echo "${GREEN}aws-cli ${NONE}"
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
 sudo ./aws/install 
-
-echo "${GREEN}gcp-cli ${NONE}"
-curl -O https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-cli-408.0.1-linux-x86_64.tar.gz
-tar -xf google-cloud-cli-408.0.1-linux-x86_64.tar.gz
-./google-cloud-sdk/install.sh
