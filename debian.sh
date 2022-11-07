fee56f2514a93ad3024f8bf60b492bb919373632
john-jparker <findingpartners115@gmail.com>
2022-11-06 21:07:10 -0600

    Update debian.sh

--- debian.sh
+++ debian.sh
@@ -17,7 +17,7 @@ sudo apt-get update -y && sudo apt-get upgrade
 echo "${GREEN}git config${NONE}"
 git config --global user.name "trevor256"
 git config --global user.email "256trevor@gmail.com"
-mkdir Desktop/gh
+mkdir gh
 
 echo "${GREEN}nvidia drivers${NONE}"
 curl -O https://us.download.nvidia.com/XFree86/Linux-x86_64/515.76/NVIDIA-Linux-x86_64-515.76.run
@@ -28,18 +28,17 @@ sudo apt-key del 7fa2af80
 curl -O https://developer.download.nvidia.com/compute/cuda/repos/debian11/x86_64/cuda-keyring_1.0-1_all.deb
 sudo dpkg -i cuda-keyring_1.0-1_all.deb
 sudo apt-get update
-sudo apt-get -y install cuda
+sudo apt-get install cuda
 
 # run on boot to install new drivers
 # sudo apt-get -y install cuda
 # grub boot init 3 install command
 
-
 echo "${GREEN}Huion tablet driver${NONE}"
 curl https://driverdl.huion.com/driver/Linux/HuionTablet_v15.0.0.89.202205241352.x86_64.deb -o huion.deb && sudo dpkg -i huion.deb
 
-echo "${GREEN}install nodejs, ffmpeg, build-essential shellcheck${NONE}"
-sudo apt-get install -y flatpak nodejs tree ffmpeg build-essential shellcheck pulseaudio pulseaudio-module-bluetooth pavucontrol bluez-firmware
+echo "${GREEN}install flatpak nodejs, tree, ffmpeg, build-essential, shellcheck, bluetooth drivers${NONE}"
+sudo apt-get install -y flatpak gnome-software-plugin-flatpak nodejs tree ffmpeg build-essential shellcheck pulseaudio pulseaudio-module-bluetooth pavucontrol bluez-firmware
 sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
 echo "${GREEN}Flatpak install Blender, Discord, Boxes, Inkscape, Krita, postman, Godot, ATMinecraft, OBS${NONE}"
@@ -49,12 +48,6 @@ com.getpostman.Postman org.kde.krita org.godotengine.GFodot \
 org.inkscape.Inkscape com.atlauncher.ATLauncher com.obsproject.Studio \
 
 echo "${GREEN}golang ${NONE}"
-wget https://golang.org/dl/go1.19.3.linux-amd64.tar.gz
-sudo tar -zxvf go1.19.3.linux-amd64.tar.gz -C /usr/local/
-echo "export PATH=/usr/local/go/bin:${PATH}" | sudo tee /etc/profile.d/go.sh
-source /etc/profile.d/go.sh
-echo "export PATH=/usr/local/go/bin:${PATH}" | sudo tee -a $HOME/.profile source
-source $HOME/.profile
 
 echo "${GREEN}aws-cli ${NONE}"
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
