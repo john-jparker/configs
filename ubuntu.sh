861825b67461477b85689e236f912ec2fd013f42
john-jparker <findingpartners115@gmail.com>
2023-01-02 17:37:24 -0600

    ubuntu and blender fix

--- ubuntu.sh
+++ ubuntu.sh
@@ -2,48 +2,39 @@
 ##  Author: https://github.com/trevor256
 ##  Usage: run on Ubuntu 22.04.1
 ##  make a usb 
-## sudo dd bs=4M if=Downloads/ubuntu.iso of=/dev/sd<?> conv=fdatasync status=progress 
+##  sudo dd bs=4M if=Downloads/ubuntu.iso of=/dev/sd<?> conv=fdatasync status=progress 
 
-## Adds green to echos
-GREEN="$(tput setaf 2)"
-NONE="$(tput sgr0)"
+# Adds green to echos
+    GREEN="$(tput setaf 2)"
+    NONE="$(tput sgr0)"
 
-sudo add-apt-repository ppa:appimagelauncher-team/stable
-sudo apt update && sudo apt upgrade
+    sudo apt update && sudo apt upgrade
 
-#Radeon™ Software for Linux® version 22.40 for Ubuntu 22.04.1
+echo "${GREEN} Install Radeon & Huion graphics tablet driver${NONE}"
+    #Radeon™ Software for Linux® version 22.40 for Ubuntu 22.04.1
+    wget https://repo.radeon.com/amdgpu-install/22.40/ubuntu/jammy/amdgpu-install_5.4.50401-1_all.deb https://driverdl.huion.com/driver/Linux/HuionTablet_v15.0.0.89.202205241352.x86_64.deb
+    sudo dpkg -i HuionTablet_v15.0.0.89.202205241352.x86_64.deb amdgpu-install_5.4.50401-1_all.deb
+    amdgpu-install -y
+
+echo "${GREEN} No HIP blender fix${NONE}"
 # https://askubuntu.com/questions/1434988/blender-3-3-does-not-recognize-my-gpu-for-hip-on-ubuntu-20-04
-wget https://repo.radeon.com/amdgpu-install/22.40/ubuntu/jammy/amdgpu-install_5.4.50401-1_all.deb
-sudo apt install amdgpu-install_5.4.50401-1_all.deb -y && amdgpu-install -y
-echo "username" read username
-sudo usermod -a -G video $username 
-sudo usermod -a -G render $username
-      
-echo "${GREEN} Snap install GO, Node, aws-cli, google-cloud-cli, nvim${NONE}"
-sudo snap install go --classic 
-sudo snap install node --classic
-sudo snap install aws-cli --classic 
-sudo snap install google-cloud-cli --classic 
-sudo snap install blender --classic
-sudo snap install discord --classic
-sudo snap install krita --classic
-sudo snap install postman --classic
-sudo snap install obs-studio --classic
-sudo snap install inkscape --classic
-sudo snap install code --classic
-sudo apt install git ffmpeg appimagelauncher -y
+    sudo usermod -a -G video $USER 
+    sudo usermod -a -G render $USER
 
-echo "${GREEN} Downloading github projects to /github dir${NONE}"
-git config --global user.name "trevor256"
-git config --global user.email "256trevor@gmail.com"
-echo "[credential "https://github.com"]" >> .gitconfig
-echo "input github token:"
-read gittoken
-sudo echo 'username = '$gittoken'' >> .gitconfig
-sudo echo 'password = '$gittoken'' >> .gitconfig
-mkdir gh
+echo "${GREEN} Snap install GO, Node, aws-cli, google-cloud-cli, blender, discord, krita, postman, obs, inkscape, VScode${NONE}"
+    sudo snap set system experimental.parallel-instances=true
+    sudo apt install git ffmpeg -y
+    sudo snap install go --classic 
+    sudo snap install node --classic
+    sudo snap install aws-cli --classic 
+    sudo snap install google-cloud-cli --classic 
+    sudo snap install blender --classic
+    sudo snap install discord --classic
+    sudo snap install krita --classic
+    sudo snap install postman --classic
+    sudo snap install obs-studio --classic
+    sudo snap install inkscape --classic
+    sudo snap install code --classic
+    sudo snap install timeshift --classic
 
-echo "${GREEN} Install Huion graphics tablet driver${NONE}"
-wget https://driverdl.huion.com/driver/Linux/HuionTablet_v15.0.0.89.202205241352.x86_64.deb && sudo dpkg -i HuionTablet_v15.0.0.89.202205241352.x86_64.deb
-
-init 6
+    init 6
