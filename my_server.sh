0a387b34dee586b12c16c163ccdcb24114a294df
john-jparker <findingpartners115@gmail.com>
2023-02-08 23:11:39 -0600

    podman and systemd

--- my_server.sh
+++ my_server.sh
@@ -5,84 +5,55 @@
 #  Linux OS (Debian)
 #
 #  Applications
-#  PXE Server, torrent, docker, cockpit, samba file share,
+#  PXE Server, torrent, podman, jellyfin, firewall, samba file share,
 ###################################################################
 
 # Adds green to echos
-      GREEN="$(tput setaf 2)"
-      NONE="$(tput sgr0)"
+GREEN="$(tput setaf 2)"
+NONE="$(tput sgr0)"
 
-echo "${GREEN} Updating..${NC}"
-      sudo add-apt-repository universe
-      sudo apt update && sudo apt upgrade -y
+echo "${GREEN}Updating${NC}" #updates
+apt install software-properties-common
+apt-add-repository non-free 
+apt-add-repository contrib
+apt update && apt upgrade -y
 
-# users and groups 
-      adduser user1
-      groupadd mygroup
-      usermod -aG mygroup olivia  #make permitions on group
+echo "${GREEN}mount drives ${NC}" #mount drives
+mkdir share
+mount /dev/sdb /share/drive1
+mount /dev/sdc /share/drive2
+mount /dev/sdd /share/drive3
 
-# Podman
-      sudo apt install podman apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
+echo "UUID=f6ed0000-828b-4ba2-af1e-e7e46c8652d5 /share/drive2 ext4 defaults 0 0" >> /etc/fstab
+echo "UUID=d343b798-b1e2-46fd-8900-170dd704429d /share/drive1 ext4 defaults 0 0" >> /etc/fstab
+echo "UUID=aef2f629-845d-4c98-9270-6bf907f07c86 /share/drive3 ext4 defaults 0 0" >> /etc/fstab
 
+#change download location and stop once done
+apt install -y ffmpeg transmission-cli #transmission-cli "" -w /share/?   kill -p -9
+
+echo "${GREEN}installing podman${NC}" #podman
+apt install -y podman
+
+echo "${GREEN}install and run jellypodman${NC}" #jellyfin podman #run as non root user 
 podman run \
  --detach \
  --label "io.containers.autoupdate=registry" \
  --name myjellyfin \
  --publish 8096:8096/tcp \
  --rm \
- --user $(id -u):$(id -g) \
+ --user $(id -u):$(id -g) \ 
  --userns keep-id \
  --volume jellyfin-cache:/cache:Z \
  --volume jellyfin-config:/config:Z \
- --mount type=bind,source=/path/to/media,destination=/media,ro=true \
+ --mount type=bind,source=/share,destination=/media,ro=true \
  docker.io/jellyfin/jellyfin:latest
 
-# cockpit managment cockpit-selinux cockpit-networkmanager
-      wget -qO - https://repo.45drives.com/key/gpg.asc | sudo gpg --dearmor -o /usr/share/keyrings/45drives-archive-keyring.gpg
-      cd /etc/apt/sources.list.d
-      sudo curl -sSL https://repo.45drives.com/lists/45drives.sources -o /etc/apt/sources.list.d/45drives.sources
-      sudo apt update
-      apt install cockpit cockpit-podman cockpit-navigator -y
+#podman generate systemd
+podman generate systemd --new --name myjellyfin > ~/.config/systemd/user/container-myjellyfin.service
+podman stop myjellyfin
+systemctl --user enable --now container-myjellyfin.service
+loginctl enable-linger $USER
+systemctl --user enable --now podman-auto-update.timer
 
-
-# jellyfin 
-      docker pull jellyfin/jellyfin
-      mkdir /jellyconfig
-      mkdir /jellycache
-      docker compose up
-#Torrent Server
-
-sudo apt-get transmission-cli transmission-common transmission-daemon
-sudo usermod -a -G debian-transmission owner
-
-sudo apt install nvidia-driver ffmpeg -y
-wget -O- https://repo.jellyfin.org/install-debuntu.sh | sudo bash
-
-transmission-cli
-
-nano /etc/ssh/sshd_config
-PermitRootLogin
-
-sudo mkdir /public /private
-sudo printf "[public]
-   comment = Public Folder
-   path = /public
-   writable = yes
-   guest ok = yes
-   guest only = yes
-   force create mode = 775
-   force directory mode = 775
-[private]
-   comment = Private Folder
-   path = /private
-   writable = yes
-   guest ok = no
-   valid users = @smbshare
-   force create mode = 770
-   force directory mode = 770
-   inherit permissions = yes" >> /etc/samba/smb.conf
-sudo groupadd smbshare
-sudo chgrp -R smbshare /private/
-sudo chgrp -R smbshare /public
-sudo chmod 2770 /private/
-sudo chmod 2775 /public
+#nvidia Driver
+apt install -y nvidia-driver 
