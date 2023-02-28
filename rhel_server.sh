79b040337c2e997f0d5b245871245b081c4a75d4
john-jparker <findingpartners115@gmail.com>
2023-02-27 23:23:22 -0600

    Create rhel_server.sh

---
+++ rhel_server.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+###########################################################
+#  Author https://github.com/trevor256
+#  script configs Red Hat server
+#  Linux OS (Red Hat
+###########################################################
