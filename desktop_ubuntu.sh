4260c7c7919f82ac86462da73c0a52cd9fd24c73
john-jparker <findingpartners115@gmail.com>
2023-02-28 17:38:09 -0600

    Update desktop_ubuntu.sh

--- desktop_ubuntu.sh
+++ desktop_ubuntu.sh
@@ -9,7 +9,7 @@ sudo apt update && sudo apt upgrade -y
 
 #Apt install git ffmpeg mysql-client-core-8.0 openjdk-17-jdk openjdk-17-jre kdenlive
 #Snap install GO, Node, aws-cli, google-cloud-cli, blender, discord, krita, postman, obs, inkscape, VScode, terraform, remmina 
-    sudo apt install git ffmpeg mysql-client-core-8.0 openjdk-17-jdk openjdk-17-jre kdenlive -y
+    sudo apt install git ffmpeg openjdk-17-jdk openjdk-17-jre kdenlive -y &
     sudo snap install go --classic 
     sudo snap install node --classic
     sudo snap install aws-cli --classic 
@@ -24,20 +24,23 @@ sudo apt update && sudo apt upgrade -y
     sudo snap install terraform --classic
     sudo snap install remmina
 
-#Downloads radeon GPU and huion tablet drivers
-    wget https://repo.radeon.com/amdgpu-install/22.40/ubuntu/jammy/amdgpu-install_5.4.50401-1_all.deb https://driverdl.huion.com/driver/Linux/HuionTablet_v15.0.0.89.202205241352.x86_64.deb
-    sudo dpkg -i HuionTablet_v15.0.0.89.202205241352.x86_64.deb amdgpu-install_5.4.50401-1_all.deb
-    amdgpu-install -y
-    
+#Downloads radeon GPU and installs
+lspci | grep 'Radeon RX' && wget https://repo.radeon.com/amdgpu-install/22.40.3/ubuntu/jammy/amdgpu-install_5.4.50403-1_all.deb
+lspci | grep 'Radeon RX' && sudo dpkg -i amdgpu-install_5.4.50403-1_all.deb && amdgpu-install -y
+
 #Fixes AMD HIP issues for blender
 #https://askubuntu.com/questions/1434988/blender-3-3-does-not-recognize-my-gpu-for-hip-on-ubuntu-20-04
     sudo usermod -a -G video $USER 
     sudo usermod -a -G render $USER
 
+#huion tablet drivers
+    wget https://driverdl.huion.com/driver/Linux/HuionTablet_v15.0.0.89.202205241352.x86_64.deb
+    sudo dpkg -i HuionTablet_v15.0.0.89.202205241352.x86_64.deb
+    
 #Add Git name and email
     git config --global user.name "trevor256"
     git config --global user.email "256trevor@gmail.com"
 
 #Clean up and Restart
-    rm HuionTablet_v15.0.0.89.202205241352.x86_64.deb amdgpu-install_5.4.50401-1_all.deb amdgpu-install_5.4.50401-1_all.deb 
+    sudo rm HuionTablet_v15.0.0.89.202205241352.x86_64.deb amdgpu-install_5.4.50401-1_all.deb amdgpu-install_5.4.50401-1_all.deb 
     init 6
