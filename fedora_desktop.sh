69cabcf0af620947db20cc58a659fcde1eef7e53
john-jparker <findingpartners115@gmail.com>
2023-02-27 23:20:30 -0600

    Create fedora_desktop.sh

---
+++ fedora_desktop.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+###########################################################
+#  Author https://github.com/trevor256
+#  script configs Fedora Desktop
+#  Linux OS (Fedora)
+###########################################################
